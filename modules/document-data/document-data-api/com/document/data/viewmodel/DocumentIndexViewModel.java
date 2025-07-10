39e1ccbf9ca623a762b857ca66d8bb747a3bed48
Beanmodf <dominhtriet47@gmail.com>
2025-07-10 21:51:18 +0700

    Define ModelView class to be utilized by the service

---
+++ modules/document-data/document-data-api/com/document/data/viewmodel/DocumentIndexViewModel.java
@@ -0,0 +1,82 @@
+package com.document.data.viewmodel;
+
+public class DocumentIndexViewModel {
+	private long documentId;
+	private long tagId;
+	private String tagName;
+	private String address;
+	private String author;
+	private long yearPublished;
+	private Date createDate;
+	private Date modifiedDate;
+	
+	public DocumentIndexViewModel() {
+	}
+	
+	public long getDocumentId() {
+	    return documentId;
+	}
+
+	public void setDocumentId(long documentId) {
+	    this.documentId = documentId;
+	}
+
+	public long getTagId() {
+	    return tagId;
+	}
+
+	public void setTagId(long tagId) {
+	    this.tagId = tagId;
+	}
+
+	public String getTagName() {
+	    return tagName;
+	}
+
+	public void setTagName(String tagName) {
+	    this.tagName = tagName;
+	}
+
+	public String getAddress() {
+	    return address;
+	}
+
+	public void setAddress(String address) {
+	    this.address = address;
+	}
+
+	public String getAuthor() {
+	    return author;
+	}
+
+	public void setAuthor(String author) {
+	    this.author = author;
+	}
+
+	public long getYearPublished() {
+	    return yearPublished;
+	}
+
+	public void setYearPublished(long yearPublished) {
+	    this.yearPublished = yearPublished;
+	}
+
+	public Date getCreateDate() {
+	    return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+	    this.createDate = createDate;
+	}
+
+	public Date getModifiedDate() {
+	    return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+	    this.modifiedDate = modifiedDate;
+	}
+
+	
+	
+}
