96d2918b2d0bfd5184b0b46e55364f6e26de7874
Beanmodf <dominhtriet47@gmail.com>
2025-07-11 10:17:43 +0700

    Add modules mvc-porlet to handles interface

---
+++ modules/document-web/src/main/java/com/document/web/constants/DocumentWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.document.web.constants;
+
+/**
+ * @author Vivobook
+ */
+public class DocumentWebPortletKeys {
+
+	public static final String DOCUMENTWEB =
+		"com_document_web_DocumentWebPortlet";
+
+}
---
+++ modules/document-web/src/main/java/com/document/web/portlet/DocumentWebPortlet.java
@@ -0,0 +1,29 @@
+package com.document.web.portlet;
+
+import com.document.web.constants.DocumentWebPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Vivobook
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=DocumentWeb",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + DocumentWebPortletKeys.DOCUMENTWEB,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class DocumentWebPortlet extends MVCPortlet {
+}
